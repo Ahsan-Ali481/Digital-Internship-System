b5f1ccb3aeb414fab6dc751990f275c97da5b912
tayyabofficial089-coder <317987417+tayyabofficial089-coder@users.noreply.github.com>
2026-07-04 08:57:10 +0000

    Render past weekly reports and supervisor rating feedback list

--- assets/js/app.js
+++ assets/js/app.js
@@ -326,3 +326,5 @@
     },
 
     clearNotification: function (notifId) {
+      let notifs = JSON.parse(localStorage.getItem('dis_notifications') || '[]');
+      notifs = notifs.filter(n => n.id !== notifId);
